c33418b56711f2a47a9c3e36679ccbc24bb339d5
Irham Maulani <irhammaulani@gmail.com>
2019-08-11 09:38:22 +0800

    fix database.php

--- config/database.php
+++ config/database.php
@@ -1,4 +1,5 @@
 <?php
+$DATABASE_URL = parse_url('postgres://hkyrhxyfenjxpq:c0d94e04406a163b9b111e4aee0124195bdea21320ba76009a216a1031474fc9@ec2-174-129-226-234.compute-1.amazonaws.com:5432/de2q8t3v3fo2im');
 
 return [
 
@@ -58,16 +59,15 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => $DATABASE_URL["host"],
+            'port' => $DATABASE_URL["port"],
+            'database' => ltrim($DATABASE_URL["path"], "/"),
+            'username' => $DATABASE_URL["user"],
+            'password' => $DATABASE_URL["pass"],
             'charset' => 'utf8',
             'prefix' => '',
-            'prefix_indexes' => true,
             'schema' => 'public',
-            'sslmode' => 'prefer',
+            'sslmode' => 'require',
         ],
 
         'sqlsrv' => [
